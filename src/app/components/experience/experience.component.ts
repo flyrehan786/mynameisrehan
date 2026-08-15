b6bdf26e02d253f46a53bc9f9438ce128694f646
Rehan Shah <flyrehan786@gmail.com>
2026-08-15 13:35:47 +0500

    ..

--- src/app/components/experience/experience.component.ts
+++ src/app/components/experience/experience.component.ts
@@ -21,7 +21,7 @@ export class ExperienceComponent {
   readonly roles: Role[] = [
     {
       period: '2023 — Now',
-      company: 'LMKR',
+      company: 'CienTech',
       role: 'Application Designer',
       current: true,
     },
